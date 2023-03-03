70eb85dec71840241fb0bd20943405fdfe04a3ab
Martin Bazik <martin@bazo.sk>
2023-03-03 09:09:49 +0100

    update

--- src/__tests__/pathMaker.test.ts
+++ src/__tests__/pathMaker.test.ts
@@ -1,35 +1,40 @@
-import path from "../";
+import path, { PathParamSpec } from "../";
 
 const paths = {
 	HOME: path("/"),
 	PROJECTS: path("/projects"),
 	PROJECTS_NEW: path("/projects/new"),
 	PROJECT: path<{ id: string }>("/projects/:id"),
-	LOGIN: path<{}, {code: number, scope: string}>("/login"),
+	LOGIN: path<PathParamSpec, { code: number; scope: string }>("/login"),
 	REGISTER: path("/register"),
 	LABELED: path("/register", "Registration"),
 };
 
 describe("path maker", () => {
-	test('simple path', () => {
-		expect(paths.HOME.toString()).toEqual("/")
-		expect(paths.HOME.toString()).toEqual(paths.HOME.path)
-		expect(paths.HOME.toURL()).toEqual("/")
-	})
+	test("simple path", () => {
+		expect(paths.HOME.toString()).toEqual("/");
+		expect(paths.HOME.toString()).toEqual(paths.HOME.path);
+		expect(paths.HOME.toURL()).toEqual("/");
+		expect(paths.HOME.path).toEqual("/");
+	});
 
-	test('path with path params', () => {
-		expect(paths.PROJECT.toString()).toEqual("/projects/:id")
-		expect(paths.PROJECT.toString()).toEqual(paths.PROJECT.path)
-		expect(paths.PROJECT.toURL({id: "123"})).toEqual("/projects/123")
-	})
+	test("path with path params", () => {
+		expect(paths.PROJECT.toString()).toEqual("/projects/:id");
+		expect(paths.PROJECT.path).toEqual("/projects/:id");
+		expect(paths.PROJECT.toString()).toEqual(paths.PROJECT.path);
+		expect(paths.PROJECT.path).toEqual(paths.PROJECT.path);
+		expect(paths.PROJECT.toURL({ id: "123" })).toEqual("/projects/123");
+	});
 
-	test('path with path query', () => {
-		expect(paths.LOGIN.toString()).toEqual("/login")
-		expect(paths.LOGIN.toString()).toEqual(paths.LOGIN.path)
-		expect(paths.LOGIN.toURL({}, {code: 123, scope: "somescope"})).toEqual("/login?code=123&scope=somescope")
-	})
+	test("path with path query", () => {
+		expect(paths.LOGIN.toString()).toEqual("/login");
+		expect(paths.LOGIN.path).toEqual("/login");
+		expect(paths.LOGIN.toString()).toEqual(paths.LOGIN.path);
+		expect(paths.LOGIN.path).toEqual(paths.LOGIN.path);
+		expect(paths.LOGIN.toURL({}, { code: 123, scope: "somescope" })).toEqual("/login?code=123&scope=somescope");
+	});
 
-	test('path has label', () => {
-		expect(paths.LABELED.label).toEqual("Registration")
-	})
+	test("path has label", () => {
+		expect(paths.LABELED.label).toEqual("Registration");
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-interface KeyValuePair {
-	[key: string]: string | number;
-}
+export type PathParamSpec = Record<string, string | number>;
 
-export class Path<P extends KeyValuePair = {}, Q extends KeyValuePair = {}> {
-	path: string;
+export class Path<P extends PathParamSpec = PathParamSpec, Q extends PathParamSpec = PathParamSpec> {
+	_path: string;
 	label: string;
 
 	constructor(path: string, label = "") {
-		this.path = path;
+		this._path = path;
 		this.label = label;
 	}
 
 	toURL(pathParams: P = {} as P, queryParams: Q = {} as Q): string {
-		let url = this.path;
+		let url = this._path;
 
 		Object.entries(pathParams).forEach(([key, value]) => {
 			url = url.replace(`:${key}`, value.toString());
@@ -23,11 +21,15 @@ export class Path<P extends KeyValuePair = {}, Q extends KeyValuePair = {}> {
 		return params ? `${url}?${params}` : url;
 	}
 
+	get path() {
+		return this.toString();
+	}
+
 	toString(): string {
-		return this.path;
+		return this._path;
 	}
 }
 
-export default <P extends KeyValuePair = {}, Q extends KeyValuePair = {}>(path: string, label = "") => {
+export default <P extends PathParamSpec = PathParamSpec, Q extends PathParamSpec = PathParamSpec>(path: string, label = "") => {
 	return new Path<P, Q>(path, label);
 };
