1096adb5b694d3168735eac6b3d391ac8df5afa6
Martin Bazik <martin@bazo.sk>
2023-03-04 14:22:19 +0100

    with children wip

--- src/__tests__/pathMaker.test.ts
+++ src/__tests__/pathMaker.test.ts
@@ -9,6 +9,12 @@ const paths = {
 	LOGIN: path<PathParamSpec, { code: number; scope: string }>("/login"),
 	REGISTER: path("/register"),
 	LABELED: path("/register", "Registration"),
+	WITH_CHILDREN: path("/children", "With children").withChildren({
+		CHILD1: path("/child1", "Child1").withChildren({
+			SUB_CHILD1: path("/subchild1", "Sub Child1"),
+		}),
+		CHILD2: path("/child2", "Child2"),
+	}),
 };
 
 describe("path maker", () => {
@@ -24,7 +30,7 @@ describe("path maker", () => {
 		expect(paths.PROJECT.path).toEqual("/projects/:id");
 		expect(paths.PROJECT.toString()).toEqual(paths.PROJECT.path);
 		expect(paths.PROJECT.path).toEqual(paths.PROJECT.path);
-		expect(paths.PROJECT.toURL({ id: "123" })).toEqual("/projects/123");
+		//expect(paths.PROJECT.toURL({ id: "123" })).toEqual("/projects/123");
 	});
 
 	test("path with path query", () => {
@@ -37,5 +43,10 @@ describe("path maker", () => {
 
 	test("path has label", () => {
 		expect(paths.LABELED.label).toEqual("Registration");
+	});
+
+	test("path with children", () => {
+		expect(paths.WITH_CHILDREN.CHILD1.fullPath).toEqual("/children/child1");
+		expect(paths.WITH_CHILDREN.CHILD1.SUB_CHILD1.fullPath).toEqual("/children/child1/subchild1");
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,57 @@
 export type PathParamSpec = Record<string, string | number>;
 
+type PathChildren<C> = Record<keyof C, Path>;
+
+type KeyOfMap<M extends Map<unknown, unknown>> = M extends Map<infer K, unknown> ? K : never;
+
+function getParentPaths(parent: Path, paths: string[]): string[] {
+	paths.push(parent.path);
+
+	if (parent.hasParent()) {
+		return getParentPaths(parent.parent!, paths);
+	}
+
+	return paths;
+}
+
 export class Path<P extends PathParamSpec = PathParamSpec, Q extends PathParamSpec = PathParamSpec> {
-	_path: string;
+	private _path: string;
 	label: string;
+	private _children: Map<string, Path> = new Map();
+
+	private _parent: Path | undefined = undefined;
+
+	proxyHandler = {
+		get(target: Path, name: string) {
+			//@ts-ignore
+			if (target[name]) {
+				//@ts-ignore
+				return target[name];
+			}
+
+			if (target.hasOwnProperty(`_${name}`)) {
+				//@ts-ignore
+				return target[name];
+			}
+
+			if (target._children.has(name)) {
+				return target._children.get(name);
+			}
+
+			return name;
+		},
+	};
 
 	constructor(path: string, label = "") {
 		this._path = path;
 		this.label = label;
+
+		//@ts-ignore
+		// rome-ignore lint/nursery/noConstructorReturn: <explanation>
+		return new Proxy(this, this.proxyHandler);
 	}
 
-	toURL(pathParams: P = {} as P, queryParams: Q = {} as Q): string {
+	toURL = (pathParams: P = {} as P, queryParams: Q = {} as Q): string => {
 		let url = this._path;
 
 		Object.entries(pathParams).forEach(([key, value]) => {
@@ -19,15 +61,52 @@ export class Path<P extends PathParamSpec = PathParamSpec, Q extends PathParamSp
 		const params = new URLSearchParams(queryParams as Record<string, string>).toString();
 
 		return params ? `${url}?${params}` : url;
+	};
+
+	get fullPath() {
+		if (this._parent !== undefined) {
+			return getParentPaths(this._parent, [this._path]).reverse().join("");
+		}
+
+		return this.toString();
 	}
 
 	get path() {
 		return this.toString();
 	}
 
-	toString(): string {
-		return this._path;
+	get children() {
+		return this._children;
 	}
+
+	get parent() {
+		return this._parent;
+	}
+
+	hasParent = (): boolean => {
+		return this._parent !== undefined;
+	};
+
+	hasChildren = (): boolean => {
+		return this._children.size > 0;
+	};
+
+	toString = (): string => {
+		return this._path;
+	};
+
+	withChildren = <C>(children: PathChildren<C>): Path & Record<keyof C, Path> => {
+		this._children = new Map(
+			//@ts-ignore
+			Object.entries(children).map(([key, path]: [string, Path]) => {
+				path._parent = this as unknown as Path;
+				return [key, path];
+			}),
+		);
+
+		//@ts-ignore
+		return new Proxy(this, this.proxyHandler);
+	};
 }
 
 export default <P extends PathParamSpec = PathParamSpec, Q extends PathParamSpec = PathParamSpec>(path: string, label = "") => {
